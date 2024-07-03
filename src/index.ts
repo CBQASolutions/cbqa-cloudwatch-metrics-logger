3388bdbe184b3ebe2d3895973564346f11dce6e2
osvaldo-cbqa <osvaldo@cbqasolutions.com>
2024-07-03 01:14:48 -0600

    add: singleton

--- src/index.ts
+++ src/index.ts
@@ -12,74 +12,89 @@ import {
   IncreaseFunctionMetricException,
 } from './exceptions/GeneralException';
 
-let cloudWatchClient: CloudWatchClient;
-let projectName: string;
-let stage: string;
+export class CloudWatchLogger {
+  private static instance: CloudWatchLogger;
+  private cloudWatchClient: CloudWatchClient;
+  private projectName: string;
+  private stage: string;
 
-export const initializeLogger = (
-  region: string = 'us-east-1',
-  projectName: string = 'DefaultMetrics',
-  stage: string = 'development'
-) => {
-  cloudWatchClient = new CloudWatchClient({ region });
-  projectName = projectName;
-  stage = stage;
-};
-
-export async function increaseFunctionMetricCount(
-  functionName: string
-): Promise<void> {
-  try {
-    const command = new PutMetricDataCommand({
-      MetricData: [
-        {
-          MetricName: CloudWatchMetricName.FUNCTION_INVOCATION,
-          Dimensions: [
-            {
-              Name: CloudWatchDimensionName.FUNCTION_NAME,
-              Value: functionName,
-            },
-          ],
-          Value: 1,
-          Unit: CloudWatchDimensionsUnit.COUNT,
-        },
-      ],
-      Namespace: `${projectName}/${stage}/Functions`,
-    });
-    await cloudWatchClient.send(command);
-  } catch (error) {
-    throw new IncreaseFunctionMetricException(error as Error);
+  private constructor(region: string, projectName: string, stage: string) {
+    this.projectName = projectName;
+    this.stage = stage;
+    this.cloudWatchClient = new CloudWatchClient({ region });
   }
-}
 
-export async function increaseErrorMetricCount(
-  functionName: string,
-  errorType: string
-): Promise<void> {
-  try {
-    const command = new PutMetricDataCommand({
-      MetricData: [
-        {
-          MetricName: CloudWatchMetricName.FUNCTION_ERROR,
-          Dimensions: [
-            {
-              Name: CloudWatchDimensionName.FUNCTION_NAME,
-              Value: functionName,
-            },
-            {
-              Name: CloudWatchDimensionName.ERROR_TYPE,
-              Value: errorType,
-            },
-          ],
-          Unit: CloudWatchDimensionsUnit.COUNT,
-          Value: 1.0,
-        },
-      ],
-      Namespace: `${projectName}/${stage}/Errors`,
-    });
+  public static getInstance(
+    region: string = 'us-east-1',
+    projectName: string = 'CustomProjectMetrics',
+    stage: string = 'development'
+  ): CloudWatchLogger {
+    if (!CloudWatchLogger.instance) {
+      CloudWatchLogger.instance = new CloudWatchLogger(
+        region,
+        projectName,
+        stage
+      );
+    }
 
-    await cloudWatchClient.send(command);
-  } catch (error) {
-    throw new IncreaseErrorMetricException(error as Error);
+    return CloudWatchLogger.instance;
+  }
+
+  public async increaseFunctionMetricCount(
+    functionName: string
+  ): Promise<void> {
+    try {
+      const command = new PutMetricDataCommand({
+        MetricData: [
+          {
+            MetricName: CloudWatchMetricName.FUNCTION_INVOCATION,
+            Dimensions: [
+              {
+                Name: CloudWatchDimensionName.FUNCTION_NAME,
+                Value: functionName,
+              },
+            ],
+            Value: 1,
+            Unit: CloudWatchDimensionsUnit.COUNT,
+          },
+        ],
+        Namespace: `${this.projectName}/${this.stage}/Functions`,
+      });
+      await this.cloudWatchClient.send(command);
+    } catch (error) {
+      throw new IncreaseFunctionMetricException(error as Error);
+    }
+  }
+
+  public async increaseErrorMetricCount(
+    functionName: string,
+    errorType: string
+  ): Promise<void> {
+    try {
+      const command = new PutMetricDataCommand({
+        MetricData: [
+          {
+            MetricName: CloudWatchMetricName.FUNCTION_ERROR,
+            Dimensions: [
+              {
+                Name: CloudWatchDimensionName.FUNCTION_NAME,
+                Value: functionName,
+              },
+              {
+                Name: CloudWatchDimensionName.ERROR_TYPE,
+                Value: errorType,
+              },
+            ],
+            Unit: CloudWatchDimensionsUnit.COUNT,
+            Value: 1.0,
+          },
+        ],
+        Namespace: `${this.projectName}/${this.stage}/Errors`,
+      });
+
+      await this.cloudWatchClient.send(command);
+    } catch (error) {
+      throw new IncreaseErrorMetricException(error as Error);
+    }
   }
 }
